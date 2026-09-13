f0502d6c2e19f2f1821d9f4d1eb3dde9c1909102
Coefficiently <Coefficiently@users.noreply.github.com>
2026-09-13 05:35:05 +0000

    Stack multiple gems on separate lines below the item instead of side-by-side inline, for a cleaner/more compact layout

--- app.js
+++ app.js
@@ -455,7 +455,7 @@
       ? `<div class="item-subline item-enchant">${item.enchant}</div>`
       : "";
     const gemsLine = (item.gems && item.gems.length > 0)
-      ? `<div class="item-subline item-gems">${item.gems.map((g) => `\u25c6 ${wowheadLink({ wowheadUrl: g.wowheadUrl, name: g.name }, "gem-link", null, "small")}`).join(" &nbsp; ")}</div>`
+      ? item.gems.map((g) => `<div class="item-subline item-gems">\u25c6 ${wowheadLink({ wowheadUrl: g.wowheadUrl, name: g.name }, "gem-link", null, "small")}</div>`).join("")
       : "";
 
     return `<li class="gear-row" style="border-left-color:${border}">
